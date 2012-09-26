bdea073d2e83d06fd73f887e248949a2f2700016
Tomokazu Bob Hirai <tomokazu.hirai@gmail.com>
2012-09-26 17:23:34 +0900

    edited metadata.

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
-maintainer       "Joakim Kolsjö"
-maintainer_email "joakim.kolsjo;AT;gmail;DOT;com"
+maintainer       "Tomokazu Hirai"
+maintainer_email "Tomokazu Hirai"
 license          "Apache 2.0"
-description      "Installs a PXE install server for installing ubuntu servers using kickstart."
-version          "0.1.2"
+description      "Install a PXE install server for installing ubuntu servers using kickstart."
+version          "0.0.1"
 
 %w{ ubuntu }.each do |os|
   supports os
